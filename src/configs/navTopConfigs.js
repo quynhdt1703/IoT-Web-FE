db3eff8a6b552953a59492e89d1deeb799965726
quynhdt1703 <quynhit203@gmail.com>
2023-12-20 19:19:23 +0700

    Web IoT

---
+++ src/configs/navTopConfigs.js
@@ -0,0 +1,15 @@
+const navTopConfigs = [
+    {
+        id: "World Air Quality",
+        name:"World Air Quality",
+        navLink:"/",
+        item: []
+    },
+    {
+        id: "Devices",
+        name:"Devices",
+        navLink:"/devices",
+        item: []
+    }
+]
+export default navTopConfigs;
